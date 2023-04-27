ff20e8b2dfc7ceaa057501b17748aff12c6b5699
Michael Aubry <michealaubrydesign@gmail.com>
2023-04-27 14:50:43 +0200

    Ajout des eventListener sur les flêches + console.log

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,33 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+console.log(slides)
+
+const leftArrow = document.querySelector(".arrow_left");
+const rightArrow = document.querySelector(".arrow_right");
+
+console.log(leftArrow)
+console.log(rightArrow)
+
+leftArrow.addEventListener("click", function () {
+	console.log("test click fleche gauche")
+});
+
+rightArrow.addEventListener("click", function () {
+	console.log("test click fleche droite")
+});
